24af2b2d5047d0cb67b6e2936e8930703cb68c03
George <georgemwazuna3@gmail.com>
2025-03-13 15:47:43 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+// Create an initial employee object
+const employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway",
+};
+
+// Function to update employee non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value }; // Returns a new object with updated key-value pair
+}
+
+// Function to update employee destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value; // Modifies the original object
+  return employee;
+}
+
+// Function to delete key from employee non-destructively
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee }; // Clone the object
+  delete newEmployee[key]; // Remove the key
+  return newEmployee; // Return new object
+}
+
+// Function to delete key from employee destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key]; // Directly modify the original object
+  return employee;
+}
